1a8e73b61db68c992cb3bd69fa2a7ffcdd7ba4a2
MStefan <mstefan@fidesol.org>
2024-06-24 14:02:57 +0200

    add files

---
+++ GrpcExample.Client/GrpcExample.Client/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcServiceExample;
+
+var channel = GrpcChannel.ForAddress("http://localhost:32771/");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = client.SayHello(new HelloRequest { Name = "World" });
+Console.WriteLine($"Greeting: {reply.Message}");
